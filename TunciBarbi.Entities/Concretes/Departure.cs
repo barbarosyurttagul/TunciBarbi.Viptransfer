906c037a2e6aff4f4b1f934abf8227c7fc2b3397
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2021-11-26 23:40:37 +0300

    Add project files.

---
+++ TunciBarbi.Entities/Concretes/Departure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VipTransfer.Entities.Concretes
+{
+    public class Departure : Location
+    {
+        public string? FlightNumber { get; set; }
+    }
+}
---
+++ TunciBarbi.Entities/Concretes/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VipTransfer.Entities.Concretes
+{
+    public class Reservation
+    {
+        public Departure DepartureId { get; set; }
+
+        public Arrival ArrivalId { get; set; }
+        public DateTime ReservationTime { get; set; }
+
+        public byte AdultNumber { get; set; }
+
+        public byte ChildNumber { get; set; }
+
+    }
+}
